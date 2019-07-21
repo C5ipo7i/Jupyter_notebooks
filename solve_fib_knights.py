6bf364661e4726029d25693570748b610215ad1e
Morgan <C5ipo7i@gmail.com>
2019-07-21 01:18:26 -0700

    Touched up the notebook to reflect the code necessary to solve the problem. Modified the code in the .py file to solve the solution from the left side instead of from the right

--- solve_fib_knights.py
+++ solve_fib_knights.py
@@ -11,6 +11,22 @@ def a_fib(N):
         l.append(l[-2]+l[-1])
     return np.array(l[2:])
 
+def return_left(N):
+    max_num = N+N #exclusive
+    fib_set = s_fib(max_num)
+    assert N in fib_set
+    if N % 2 == 0:
+        return int(N / 2)
+    else:
+        fib_arr = a_fib(max_num)
+        index = np.where(fib_arr == N)[0]
+        print('index',index)
+        # Check above and below for % 2 == 0
+        if fib_arr[index+1] % 2 == 0:
+            return int((fib_arr[index+1] / 2)[0])
+        else:
+            return int((fib_arr[index-1] / 2)[0])
+
 def fibs_between_numbers(A,N,fibs):
     mask = np.logical_and(fibs > A,fibs <= N+A,fibs / A != 2)
     return fibs[mask]
@@ -26,21 +42,29 @@ def first_single(N,max_num,fib_arr):
         index += 1
 
 # list is populated in reverse order
-def solve_knights(N,chair_position):
+def solve_knights_left(N,chair_position):
     max_num = N+N #exclusive
     used = set()
-    start = time.time()
+    L = return_left(N)
     fib_arr = a_fib(max_num)
-    end,next_num = first_single(N,max_num,fib_arr)
+    end,pen_end = first_single(N,max_num,fib_arr)
     used.add(end)
-    used.add(next_num)
-    solution = [end,next_num]
+    used.add(pen_end)
+    used.add(L)
+    solution_right = [pen_end,end]
     episode = 0
-    repeats = next_num
-    while len(solution) < chair_position+1: # N
+    solution = [L]
+    # compute max possibility
+    repeats = L
+    next_num = L
+    while len(solution) < chair_position: #N-2 for solving full list
         poss_fibs = fibs_between_numbers(next_num,N,fib_arr)
         poss_nums = np.subtract(poss_fibs,np.full(poss_fibs.shape[0],next_num,dtype=object))
+#         print('solution',solution)
+#         print('poss_fibs',poss_fibs)
+#         print('poss_nums',poss_nums)
         for poss in reversed(poss_nums):
+#             print('poss',poss)
             if poss in used:
                 pass
             else:
@@ -54,17 +78,21 @@ def solve_knights(N,chair_position):
             break
         else:
             repeats = solution[-1]
-    return solution[-1]
+#         if episode % 100000 == 0:
+#             toc = time.time()
+#             print("Episode {}, len(solution) = {}, time taken so far {} seconds".format(episode,len(solution),(toc-start)))
+#             break
+    return solution[-1]#solution+solution_right for returning full list
 
 def main():
-    N = 144 # for testing purposes
+    # N = 144 # for testing purposes
     full_N = 99194853094755497
     chair_position_left = 1e+16
     # Because we are building the array from the reverse position
-    chair_position_right = int(full_N - chair_position_left)
+    # chair_position_right = int(full_N - chair_position_left)
 
     tic = time.time()
-    solution = solve_knights(full_N,chair_position_right)
+    solution = solve_knights_left(full_N,chair_position_left)
     toc = time.time()
     # Solution is returned as a single number instead of the full list, due to size
     print("The Solution {} took {} seconds".format(solution,(toc-tic)))
